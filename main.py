571184557703ca083c1ebaec6eb4380367568736
sviatlanapoz <158464974+sviatlanapoz@users.noreply.github.com>
2024-03-04 13:24:14 +0000

    Added comments

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+#Importing pandas module 
 import pandas
 
+#Opening csv file containing nato phonetic alphabet 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+#Creates a dictionary from a DataFrame, mapping row.letter to row.code for each row.
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #List of the phonetic code words from a word that the user inputs.
-
 def generate_phonetic():
     word = input("Enter a word: ").upper()
     try:
@@ -15,5 +17,5 @@ def generate_phonetic():
         generate_phonetic()
     else:
         print(result)
-
+#Function call
 generate_phonetic()
